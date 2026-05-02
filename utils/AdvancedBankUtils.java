35d61c94a227c48e8119126130c7f8d39bb0c515
SAMUEL MESERET <samiawkcole@gmail.com>
2026-05-02 10:12:44 +0300

    Create AdvancedBankUtils.java

---
+++ utils/AdvancedBankUtils.java
@@ -0,0 +1,9 @@
+package bank.utils;
+
+public class AdvancedBankUtils extends BankUtils {
+
+ 
+    public static void bankInfo() {
+        System.out.println("Advanced Banking Services Activated.");
+    }
+}
