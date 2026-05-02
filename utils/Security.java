5e1bab62349f825eee9868d00f75dc89ddfe463f
SAMUEL MESERET <samiawkcole@gmail.com>
2026-05-02 10:16:11 +0300

    Create Security.java

---
+++ utils/Security.java
@@ -0,0 +1,10 @@
+package bank.utils;
+
+
+public final class Security {
+
+
+    public final void secureSystem() {
+        System.out.println("Security system is active and protected.");
+    }
+}
