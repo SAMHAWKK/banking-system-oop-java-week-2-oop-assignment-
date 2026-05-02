403c433c29141a37f6b0715397b53e7171e4c1b4
SAMUEL MESERET <samiawkcole@gmail.com>
2026-05-02 10:18:53 +0300

    Update Account.java

--- Model/Account.java
+++ Model/Account.java
@@ -4,19 +4,19 @@ public class Account {
     protected String accountHolder;
     protected double balance;
 
-    // Constructor
+    
     public Account(String accountHolder, double balance) {
         this.accountHolder = accountHolder;
         this.balance = balance;
     }
 
-    // Display method
+    
     public void display() {
         System.out.println("Name: " + accountHolder);
         System.out.println("Balance: " + balance);
     }
 
-    // Final method (cannot be overridden)
+    
     public final void bankPolicy() {
         System.out.println("Bank policy is fixed for all accounts.");
     }
